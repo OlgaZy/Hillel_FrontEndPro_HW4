b8551dabb57870ca1fd276e11171b528d69297a4
Olga Zykova <olgazykova27@gmail.com>
2024-12-17 23:58:43 +0200

    first commit
    5-digit number, validation

---
+++ js.js
@@ -0,0 +1,18 @@
+let fiveDigitNumber = prompt('Enter 5-digit number:');
+
+if (fiveDigitNumber !== null && !isNaN(fiveDigitNumber) && Number.isInteger(+fiveDigitNumber)){   
+    fiveDigitNumber = Math.abs(+fiveDigitNumber);
+    let str = fiveDigitNumber.toString();
+    if (str.length === 5){
+        let fiveDigitNumberArray = fiveDigitNumber.toString().split('');
+        let fiveDigitNumberSrt = fiveDigitNumberArray.join(' ');
+        document.getElementById("income").innerHTML = fiveDigitNumber;
+        document.getElementById("number").innerHTML = fiveDigitNumberSrt;
+    }
+    else{
+        alert("It's not a 5-digit number");
+    }
+} 
+else {
+    alert("Invalid number entered");
+} 
