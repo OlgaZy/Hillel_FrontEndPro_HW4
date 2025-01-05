7ac285d49dfeaaabd871c99691a0e526939eaaa9
Olga Zykova <olgazykova27@gmail.com>
2025-01-05 23:57:15 +0200

    Update js.js
    validation fix

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
 let fiveDigitNumber = prompt('Enter 5-digit number:');
 
-if (fiveDigitNumber !== null && !isNaN(fiveDigitNumber) && Number.isInteger(+fiveDigitNumber)){   
+if (fiveDigitNumber?.trim() && !isNaN(fiveDigitNumber) && Number.isInteger(+fiveDigitNumber)){   
     fiveDigitNumber = Math.abs(+fiveDigitNumber);
     let str = fiveDigitNumber.toString();
     if (str.length === 5){
         let fiveDigitNumberArray = str.split('');
         let fiveDigitNumberSrt = fiveDigitNumberArray.join(' ');
-        document.getElementById("income").innerHTML = fiveDigitNumber;
-        document.getElementById("number").innerHTML = fiveDigitNumberSrt;
     }
     else{
         alert("It's not a 5-digit number");
